9c7c940342b03cc4bcca8c1192478cb9c3e83196
Markus Ort <markus@ort.dev>
2025-05-04 14:42:50 +0200

    make docker build only run on tag push

--- tools/docker-build.rs
+++ tools/docker-build.rs
@@ -20,15 +20,6 @@ pub fn main() {
         std::process::exit(1);
     }
 
-    let git_tag_str =
-        String::from_utf8(tag.stdout).expect("UTF-8 Formatting Error");
-    if git_tag_str.trim() == env!("CARGO_PKG_VERSION") {
-        println!("Package has same Version, skipping build.");
-        return;
-    }
-
-    println!("Package has different Version, initiating new Build.");
-
     let status = Command::new("docker")
         .env("DOCKER_BAKE", "1")
         .args([
